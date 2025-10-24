f67c02516926b638de86d5d52c5f2777f7974fda
m.ali <majid.live@gmail.com>
2025-10-24 18:49:05 +0330

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,54 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState("black");
 
   return (
-    <>
-      <div className="flex flex-col justify-center items-center">
-        <div>
-          <h1 className="font-bold text-[64px]">Counter</h1>
-        </div>
-        <div>
-          <span
-            className="text-[96px] font-semibold"
-            id="countalue text-black text-16"
+    <div className="flex flex-col justify-center items-center">
+      <div>
+        <h1 className="font-bold text-[64px]">Counter</h1>
+      </div>
+      <div>
+        <span
+          className="text-[96px] font-semibold"
+          id="countalue text-black text-16"
+        >
+          {count}
+        </span>
+        <div className="flex flex-row gap-6">
+          <button
+            onClick={() => {
+              setCount(count - 1);
+            }}
+            id="decrease"
+            className="border-2 border-black px-3 rounded-md"
           >
-            0
-          </span>
-          <div className="flex flex-row gap-6">
-            <button
-              id="decrease"
-              className="border-2 border-black px-3 rounded-md"
-            >
-              DECREASE
-            </button>
-            <button
-              id="reset"
-              className="border-2 border-black px-3 rounded-md"
-            >
-              RESET
-            </button>
-            <button
-              id="increase"
-              className="border-2 border-black px-3 rounded-md"
-            >
-              INCREASE
-            </button>
-          </div>
+            DECREASE
+          </button>
+          <button
+            onClick={() => {
+              setCount((count = 0));
+            }}
+            id="reset"
+            className="border-2 border-black px-3 rounded-md"
+          >
+            RESET
+          </button>
+          <button
+            onClick={() => {
+              setCount(count + 1);
+            }}
+            id="increase"
+            className="border-2 border-black px-3 rounded-md"
+          >
+            INCREASE
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
